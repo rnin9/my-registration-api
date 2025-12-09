ab3af9708158018efcf2ebf7dbb7d94dc89d7b02
rnin9 <rkdalswn1209@naver.com>
2025-12-09 15:19:53 +0900

    chore: db Migration

--- db/migrate/20251209055448_fix_is_destroyed_defaults.rb
+++ db/migrate/20251209055448_fix_is_destroyed_defaults.rb
@@ -1,4 +1,30 @@
 class FixIsDestroyedDefaults < ActiveRecord::Migration[7.1]
-  def change
+  def up
+    # 1. users 테이블
+    change_column_default :users, :is_destroyed, false
+    User.where(is_destroyed: nil).update_all(is_destroyed: false)
+    change_column_null :users, :is_destroyed, false
+
+    # 2. tests 테이블
+    change_column_default :tests, :is_destroyed, false
+    Test.where(is_destroyed: nil).update_all(is_destroyed: false)
+    change_column_null :tests, :is_destroyed, false
+
+    # 3. courses 테이블
+    change_column_default :courses, :is_destroyed, false
+    Course.where(is_destroyed: nil).update_all(is_destroyed: false)
+    change_column_null :courses, :is_destroyed, false
+  end
+
+  def down
+    # Rollback (필요시)
+    change_column_null :users, :is_destroyed, true
+    change_column_default :users, :is_destroyed, nil
+
+    change_column_null :tests, :is_destroyed, true
+    change_column_default :tests, :is_destroyed, nil
+
+    change_column_null :courses, :is_destroyed, true
+    change_column_default :courses, :is_destroyed, nil
   end
 end
