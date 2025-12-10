b946d4e63cdb0395965eedf9a6fcf4e61bd1355a
rnin9 <rkdalswn1209@naver.com>
2025-12-10 15:47:09 +0900

    fix: migration

--- db/migrate/20251209063748_create_payments.rb
+++ db/migrate/20251209063748_create_payments.rb
@@ -1,7 +1,6 @@
 class CreatePayments < ActiveRecord::Migration[7.1]
   def change
     create_table :payments, id: :uuid do |t|
-      t.uuid :user_id, null: false
       t.integer :amount, null: false
       t.string :method
       t.string :status, null: false
@@ -13,11 +12,11 @@ class CreatePayments < ActiveRecord::Migration[7.1]
       t.date :valid_from, null: false
       t.date :valid_to, null: false
       t.boolean :is_destroyed, default: false, null: false
+      t.references :actant, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
 
-    add_index :payments, :user_id
     add_index :payments, :status
     add_index :payments, :target_type
     add_index :payments, :target_id
@@ -26,6 +25,5 @@ class CreatePayments < ActiveRecord::Migration[7.1]
     add_index :payments, :valid_from
     add_index :payments, :valid_to
 
-    add_foreign_key :payments, :users, column: :user_id
   end
 end
